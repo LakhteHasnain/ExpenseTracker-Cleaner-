14647fa23f7ed32798684ce85a19a6c17e35e063
LakhteHasnain <pakumpire786@gmail.com>
2025-11-03 22:07:25 +0500

    env path update

--- src/config/db.py
+++ src/config/db.py
@@ -4,12 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv('C:\\Users\\PMLS\\Desktop\\Dev\\backend\\ExpenseTracker(Cleaner)\\.env')
-
-# Database connection URL
-DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-
+DATABASE_URL = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set.")
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
